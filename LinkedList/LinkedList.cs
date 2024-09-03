cf237cf15f04e7801d0f66799baa9d7f5a9da0f3
deltaforce26 <ahron052715@gmail.com>
2024-09-03 17:09:27 +0300

    Add project files.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace data_structures
+{
+    internal class Node
+    {
+        public Node(int data)
+        {
+            Value = data;
+        }
+        private int Value;
+        private Node? Next = null;
+
+        public void SetValue(int data)
+        {
+            Value = data;
+        }
+
+        public int GetValue()
+        {
+            return Value;
+        }
+
+        public void SetNext(Node? node)
+        {
+            Next = node;
+        }
+
+        public Node? GetNext()
+        {
+            if (Next == null)
+            {
+                return null;
+            }
+            return Next;
+        }
+    }
+
+    internal class LinkedList
+    {
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        private Node? Head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (Head == null)
+                Head = node;
+            else
+            {
+                Node current = Head;
+                while (current.GetNext() != null)
+                {
+                    current = current.GetNext();
+                }
+                current.SetNext(node);
+            }
+        }
+
+        public string Display()
+        {
+            if (Head == null)
+                return "";
+
+            string nodes = string.Empty;
+            Node current = Head;
+            while (current != null)
+            {
+                nodes += current.GetValue() + "->";
+            }
+            return nodes;
+        }
+
+        public int Length()
+        {
+            if (Head == null)
+                return 0;
+            int length = 0;
+            Node current = Head;
+            while (current != null)
+            {
+                length++;
+            }
+            return length;
+        }
+
+        public void RemoveValue(int data)
+        {
+            if (Head == null)
+                return;
+
+            Node previous = Head;
+            Node current = Head;
+            while (current != null)
+            {
+                int value = current.GetValue();
+                if (value == data)
+                {
+                    if (current.GetNext() != null)
+                        previous.SetNext(current.GetNext());
+                    else
+                        previous.SetNext(null);
+
+                    current.SetNext(null);
+                }
+                previous = current;
+                current = current.GetNext();
+            }
+        }
+    }
+}
